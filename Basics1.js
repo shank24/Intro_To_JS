6c5d2c2edb6c6104082edc402a64fb4d2d84d34a
Shanky Kalra <shankkalra@gmail.com>
2024-04-04 19:29:30 -0300

    Add Let

--- Basics1.js
+++ Basics1.js
@@ -3,6 +3,15 @@ console.log("Hello World")
 // var - is to declare variable
 //(let, const) - are available from ES6 Version
 
+//Declare Variable
 let a=5
 console.log(a)
+
+//To check the datatype of a variable
 console.log(typeof(a))
+
+let c = "Javascript"
+console.log(typeof(c));
+
+let bool = true
+console.log(typeof(bool));
