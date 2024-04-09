95f47d7fb245f85bd12cdd665b2860172e985df8
Shanky Kalra <shankkalra@gmail.com>
2024-04-08 21:46:09 -0300

    Array Operations

--- Basics3.js
+++ Basics3.js
@@ -5,6 +5,19 @@ marks = new Array(10,20,30,40,50,60)
 //Shortcut
 var marks1 = [1,2,3,4,5,6]
 console.log(marks1[1]);
+console.log(marks);
 
 //Length
 console.log(marks.length);
+
+//Append at the end.
+marks.push(55)
+console.log(marks);
+
+//Delete from the end
+marks.pop()
+console.log(marks);
+
+//Add at the beginning
+marks.unshift(85)
+console.log(marks);
