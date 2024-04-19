118ad7578a9a99071ca84655c218c62651d7ee04
Shanky Kalra <shankkalra@gmail.com>
2024-04-18 21:39:40 -0300

    Function

---
+++ Basics5.js
@@ -0,0 +1,17 @@
+//Function Demo
+
+function add (a,b)
+{
+return a+b
+}
+let value = add(1,5)
+console.log(value);
+
+//Anonymous Function
+let sum = function(c,d){
+    return c+d
+}
+
+//Anonymous Function Other Way
+let sum1 = (c,d)=> c+d
+console.log(sum1(5,9))
