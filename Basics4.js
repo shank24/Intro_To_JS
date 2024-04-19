db9393abeee0a33d08e06020791fbd9207f3832e
Shanky Kalra <shankkalra@gmail.com>
2024-04-18 21:27:26 -0300

    Sorting Bubble

--- Basics4.js
+++ Basics4.js
@@ -1,6 +1,17 @@
-//Sorting Array
-
+//Sorting String Array
 var fruit = ["Banana","Mango","Apple","Orange"]
-
 fruit.sort()
 console.log(fruit);
+
+//Sorting Integer
+var number = [12,003,19,16,14]
+console.log(number.sort());
+
+//Traditional Way
+number.sort(function(a,b){
+    return a-b
+})
+
+//Anonymous Function
+console.log(number.sort( (a,b)=> a-b));
+
