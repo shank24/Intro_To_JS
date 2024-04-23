3e0aa581ac0315862fffcad938b58d8b7b3fba98
Shanky Kalra <shankkalra@gmail.com>
2024-04-23 20:55:35 -0300

    Object

--- Intro_Subjects/Basics7.js
+++ Intro_Subjects/Basics7.js
@@ -2,12 +2,33 @@
 //Object is a collection of a properties 
 
 let person ={
-    firstName : "Jim",
-    lastName : "Jacobs"
+    firstName : "Jim ",
+    lastName : "Jacobs",
+    age : 25,
+    fullName: function(){
+        console.log(this.firstName + this.lastName);
+    }
 }
 
+//Specific to Function Value
+console.log(person.fullName());
+//Printing an Object
 console.log(person.lastName);
 console.log(person['lastName']);
 
+//Adding a property
 person.gender = 'Male'
 console.log(person);
+
+//Deleting an object
+delete person.gender
+console.log(person);
+
+//Checking Property Present
+console.log('gender' in person);
+
+//Printing Values of all JS Object
+for(let key in person)
+{
+    console.log(person[key]);
+}
